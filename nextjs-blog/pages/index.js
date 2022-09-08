631f54ed15f8d0026e62c150716df61ee8dab066
mushyoom <mushyoom@gmail.com>
2022-09-08 23:44:25 +0900

    page navigation

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Learn Next.js!</a>
+          Welcome to <Link href="post-details"> Next.js!</Link>
         </h1>
 
         <p className="description">
